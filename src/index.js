8f8784070cf76446ce96f3c132ccf8e41a56bc74
Napselc <napselc.abhi@gmail.com>
2023-06-28 10:10:28 +0200

    Push changes in index.js

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { CategoriesProvider } from './contexts/categories.context';
 import { CartProvider } from './contexts/cart.context';
 import {Elements} from "@stripe/react-stripe-js"
 import { stripePromise } from './utils/stripe/stripe.utils';
-
+//
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
